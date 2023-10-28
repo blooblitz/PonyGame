398dc621b2359cd902e9569267a1159e02a764ad
ark <ohgodaowl@gmail.com>
2023-10-28 04:56:43 +0200

    test

---
+++ node_2d.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+const SPEED = 250.0
+
+func _physics_process(delta):
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * SPEED
+
+	move_and_slide()
